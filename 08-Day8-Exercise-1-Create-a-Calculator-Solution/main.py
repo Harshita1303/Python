c23ff85b052d5ce16f4146395788cc2c2d10474b
Harshita Halli <110682759+Harshita1303@users.noreply.github.com>
2024-07-16 21:13:24 +0530

    Update main.py

--- 08-Day8-Exercise-1-Create-a-Calculator-Solution/main.py
+++ 08-Day8-Exercise-1-Create-a-Calculator-Solution/main.py
@@ -5,3 +5,5 @@ print("The value of", a, "+", 3, "is: ", a + b)
 print("The value of", a, "-", 3, "is: ", a - b)
 print("The value of", a, "*", 3, "is: ", a * b)
 print("The value of", a, "/", 3, "is: ", a / b)
+
+## [Next Lesson>>](https://github.com/Harshita1303/Python/blob/main/09-Day9-Typecasting-in-Python/.tutorial/Tutorial.md)
